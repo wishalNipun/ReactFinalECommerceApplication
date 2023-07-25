07ac0508768131d926f0f402e41c017b0646d1fc
wishalNipun <wishal1974@gmail.com>
2023-07-25 12:55:41 +0530

    order form frontend updated

--- frontend/src/component/DashBoardContent/OrderForm.jsx
+++ frontend/src/component/DashBoardContent/OrderForm.jsx
@@ -7,18 +7,35 @@ import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import Cart from './Cart';
 import CardComponent from '../card/CardComponent';
-import img from '../../assets/imgs/shoe1.jpg';
 import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+import img from '../../assets/imgs/shoe1.jpg';
+import img2 from '../../assets/imgs/shoe2.jpg';
 
 export const OrderForm = () => {
+
+  const [cusId, setCusId] = React.useState('');
+
+  const handleChange = (event) => {
+    setCusId(event.target.value);
+  };
   return (
     <div >
       
   
         <Grid container sx={{ mt: 3}}>
-            <Grid xs={7} style={{ border: '2px solid'}}>
+            <Grid xs={7} style={{ }}>
 
-              <Container style={{ border: '2px solid green'}}>
+              <Container style={{ }}>
 
                 <Paper sx={{ p: '2px 4px', display: 'flex', alignItems: 'center'}}
 >
@@ -36,14 +53,14 @@ export const OrderForm = () => {
                 </Paper>
               </Container>
              
-              <Container style={{ border: '2px solid green','height':'550px','overflowX':'auto'} } sx={{ mt: 1}}>
+              <Container style={{'height':'550px','overflowX':'auto'} } sx={{ mt: 1}}>
               <Stack direction="row" spacing={2} useFlexGap flexWrap="wrap" justifyContent="space-between">
                 <CardComponent  imageSrc={img}/>
+                <CardComponent  imageSrc={img2}/>
+                <CardComponent  imageSrc={img2}/>
                 <CardComponent  imageSrc={img}/>
                 <CardComponent  imageSrc={img}/>
-                <CardComponent  imageSrc={img}/>
-                <CardComponent  imageSrc={img}/>
-                <CardComponent  imageSrc={img}/>
+                <CardComponent  imageSrc={img2}/>
                 <CardComponent  imageSrc={img}/>
               </Stack>
                 
@@ -51,11 +68,39 @@ export const OrderForm = () => {
             
               </Container>
             </Grid>
-            <Grid  xs={5} style={{'border':'2px solid red','height':'600px'}}>
-              <Cart/>
+            <Grid  xs={5} >
+              <Container style={{'height':'420px'}}>
+                  <FormControl>
+                      <InputLabel size="small"  >Customer Id</InputLabel>
+                      <Select
+                    
+                        value={cusId}
+                        label="Customer Id"
+                        onChange={handleChange}
+                        size="small" 
+                        
+                      >
+                        <MenuItem selected value="Random Customer">
+                          <em>Random Customer</em>
+                        </MenuItem>
+                        <MenuItem value={10}>C001</MenuItem>
+                        <MenuItem value={20}>C002</MenuItem>
+                        <MenuItem value={30}>C003</MenuItem>
+                      </Select>
+                      <FormHelperText>Select If Guest Customer : Random Customer</FormHelperText>
+                </FormControl>
+                <Cart/>
+              </Container>
+              
 
-              <Container style={{ border: '2px solid grey'}}>
-                
+              <Container style={{ 'height':'193px'}}>
+              
+                <Stack direction="column" spacing={2}>
+                  <h5>Sub-Total :</h5>
+                  <TextField  id="outlined-basic" label="Cash" variant="outlined"  size="small" type='number'/>
+                  <h5>Balance :</h5>
+                  <Button variant="contained" endIcon={<SendIcon />}>Place Order</Button>
+                </Stack>
               </Container>
              
             </Grid>
--- frontend/src/component/card/CardComponent.jsx
+++ frontend/src/component/card/CardComponent.jsx
@@ -8,7 +8,7 @@ import Typography from '@mui/material/Typography';
 
 export default function CardComponent({ imageSrc }) {
   return (
-    <Card sx={{ maxWidth: 200 }}>
+    <Card sx={{ width: 200 }}>
       <CardMedia
         sx={{ height: 140 }}
         image={ imageSrc }
@@ -16,15 +16,15 @@ export default function CardComponent({ imageSrc }) {
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard
+         Nike
         </Typography>
         <Typography variant="body2" color="text.secondary">
           Lizards ar
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
+      <Button size="small">Add To Cart</Button>
+       
       </CardActions>
     </Card>
   );
